b00a4791f57b8fa50821835a44bb4dc50aab172b
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-29 21:18:56 +0800

    Update TemplateView.java

--- src/main/java/com/vaneks/crud/view/TemplateView.java
+++ src/main/java/com/vaneks/crud/view/TemplateView.java
@@ -6,10 +6,7 @@ import java.util.Scanner;
 
 public abstract class TemplateView {
     private Scanner scanner = new Scanner(System.in);
-    public void templateMethod(String name) {
 
-        view(name) ;
-    }
     public void view(String name) {
 
         System.out.print("Выберите действие, и нажмите Enter: \n" +
